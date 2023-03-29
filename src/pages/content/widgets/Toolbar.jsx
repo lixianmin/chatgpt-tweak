f029fab7bb2956408943718d405fa0d721062b0e
lixianmin <lixianmin@gmail.com>
2023-03-29 19:42:52 +0800

    终于可以排版喽

--- src/pages/content/widgets/Toolbar.jsx
+++ src/pages/content/widgets/Toolbar.jsx
@@ -2,7 +2,7 @@
 
 import ShadowBootstrap from "@src/core/widgets/ShadowBootstrap.jsx";
 import ToolbarEnable from "@pages/content/widgets/ToolbarEnable.jsx";
-import { ButtonGroup } from "solid-bootstrap";
+import { Col, Form, Row } from "solid-bootstrap";
 import ToolbarPrompts from "@pages/content/widgets/ToolbarPrompts.jsx";
 
 /********************************************************************
@@ -15,10 +15,16 @@ import ToolbarPrompts from "@pages/content/widgets/ToolbarPrompts.jsx";
 export default function Toolbar(props) {
   return <>
     <ShadowBootstrap id={props.id}>
-      <ButtonGroup class="mb-2">
-        <ToolbarEnable />
-        <ToolbarPrompts />
-      </ButtonGroup>
+      <Form>
+        <Row>
+          <Col xs={3}>
+            <ToolbarEnable />
+          </Col>
+          <Col xs={2}>
+            <ToolbarPrompts />
+          </Col>
+        </Row>
+      </Form>
     </ShadowBootstrap>
   </>;
 }
