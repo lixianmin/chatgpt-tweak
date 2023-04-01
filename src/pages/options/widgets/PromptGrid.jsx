c91cbcf8e3fc2afa52c6fa664545b049ab8524a2
lixianmin <lixianmin@gmail.com>
2023-04-01 15:22:02 +0800

    能用，但明显colNum非常大，所以应该不需要使用它

--- src/pages/options/widgets/PromptGrid.jsx
+++ src/pages/options/widgets/PromptGrid.jsx
@@ -1,8 +1,8 @@
 "use strict";
 import usePrompts from "@src/dao/Prompts.js";
-import { For } from "solid-js";
+import { onMount } from "solid-js";
 import PromptItem from "@pages/options/widgets/PromptItem.jsx";
-import { Form } from "solid-bootstrap";
+import { Card, Col, Form, Row } from "solid-bootstrap";
 import AddPromptItem from "@pages/options/widgets/AddPromptItem.jsx";
 
 /********************************************************************
@@ -14,13 +14,42 @@ import AddPromptItem from "@pages/options/widgets/AddPromptItem.jsx";
 
 export default function PromptGrid() {
   const prompts = usePrompts();
+  const cardsData = prompts.getPromptList().slice().reverse();
+  let colNum = 1;
+
+  function calculateColNum() {
+    const pageWidth = window.innerWidth;
+    const cardWidthInt = 1;
+    const cardMarginInt = 1;
+    colNum = Math.floor(pageWidth / (cardWidthInt + 2 * cardMarginInt));
+    console.log("innerWidth=", pageWidth, "colNum=", colNum);
+  }
+
+  onMount(() => {
+    calculateColNum();
+    window.addEventListener("resize", calculateColNum);
+  });
+
+  calculateColNum();
+  const rowNum = Math.ceil(cardsData.length / colNum);
+  const rows = Array.from({ length: rowNum }, (_, rowIndex) => {
+    const start = rowIndex * colNum;
+    const end = start + colNum;
+    return cardsData.slice(start, end).map((prompt, colIndex) => (
+      <Col md="auto">
+        <Card>
+          <PromptItem prompts={prompts} reverseIndex={start + colIndex}></PromptItem>
+        </Card>
+      </Col>
+    ));
+  });
 
   return <>
     <Form>
       <AddPromptItem prompts={prompts} />
-      <For each={prompts.getPromptList().slice().reverse()}>{(prompt, reverseIndex) => {
-        return <PromptItem prompts={prompts} reverseIndex={reverseIndex()}></PromptItem>;
-      }}</For>
+      {rows.map((cols) => {
+        return <Row>{cols}</Row>;
+      })}
     </Form>
   </>;
 }
