3b882e691c4764c12f3ac90d5c9411181445e4d5
lixianmin <lixianmin@gmail.com>
2023-04-01 15:34:00 +0800

    通过在一行中把所有的column都放进来，然后设置 md='auto'，可以进行自动排版

--- src/pages/options/widgets/PromptGrid.jsx
+++ src/pages/options/widgets/PromptGrid.jsx
@@ -1,9 +1,9 @@
 "use strict";
 import usePrompts from "@src/dao/Prompts.js";
-import { onMount } from "solid-js";
 import PromptItem from "@pages/options/widgets/PromptItem.jsx";
-import { Card, Col, Form, Row } from "solid-bootstrap";
+import { Col, Form, Row } from "solid-bootstrap";
 import AddPromptItem from "@pages/options/widgets/AddPromptItem.jsx";
+import { For } from "solid-js";
 
 /********************************************************************
  created:    2023-03-28
@@ -14,42 +14,19 @@ import AddPromptItem from "@pages/options/widgets/AddPromptItem.jsx";
 
 export default function PromptGrid() {
   const prompts = usePrompts();
-  const cardsData = prompts.getPromptList().slice().reverse();
-  let colNum = 1;
-
-  function calculateColNum() {
-    const pageWidth = window.innerWidth;
-    const cardWidthInt = 1;
-    const cardMarginInt = 1;
-    colNum = Math.floor(pageWidth / (cardWidthInt + 2 * cardMarginInt));
-    console.log("innerWidth=", pageWidth, "colNum=", colNum);
-  }
-
-  onMount(() => {
-    calculateColNum();
-    window.addEventListener("resize", calculateColNum);
-  });
-
-  calculateColNum();
-  const rowNum = Math.ceil(cardsData.length / colNum);
-  const rows = Array.from({ length: rowNum }, (_, rowIndex) => {
-    const start = rowIndex * colNum;
-    const end = start + colNum;
-    return cardsData.slice(start, end).map((prompt, colIndex) => (
-      <Col md="auto">
-        <Card>
-          <PromptItem prompts={prompts} reverseIndex={start + colIndex}></PromptItem>
-        </Card>
-      </Col>
-    ));
-  });
-
+  // 通过在一行中把所有的column都放进来，然后设置 md='auto'，可以进行自动排版
   return <>
     <Form>
-      <AddPromptItem prompts={prompts} />
-      {rows.map((cols) => {
-        return <Row>{cols}</Row>;
-      })}
+      <Row>
+        <Col md="auto">
+          <AddPromptItem prompts={prompts} />
+        </Col>
+        <For each={prompts.getPromptList().slice().reverse()}>{(prompt, reverseIndex) => {
+          return <Col md="auto">
+            <PromptItem prompts={prompts} reverseIndex={reverseIndex()}></PromptItem>
+          </Col>;
+        }}</For>
+      </Row>
     </Form>
   </>;
 }
