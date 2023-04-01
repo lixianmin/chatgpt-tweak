1bacbe3c69c210b28b234889cf12703214a37ebb
lixianmin <lixianmin@gmail.com>
2023-04-01 10:54:25 +0800

    Alert控件对signal响应有问题，改为Badge控件

--- src/pages/options/widgets/AddPromptItem.jsx
+++ src/pages/options/widgets/AddPromptItem.jsx
@@ -1,6 +1,6 @@
 "use strict";
 
-import { Alert, Button, Card, Col, Form, Row } from "solid-bootstrap";
+import { Badge, Button, Card, Col, Form, Row } from "solid-bootstrap";
 import { createSignal } from "solid-js";
 
 /********************************************************************
@@ -13,6 +13,8 @@ import { createSignal } from "solid-js";
 export default function AddPromptItem(props) {
   const prompts = props.prompts;
   const [showWarning, setShowWarning] = createSignal(false);
+  const [disableAddButton, setDisableAddButton] = createSignal(true);
+  const [nameSignal, setNameSignal] = createSignal("");
 
   let nameControl;
   let textControl;
@@ -20,17 +22,28 @@ export default function AddPromptItem(props) {
   function onInputName() {
     const name = nameControl.value.trim();
     const last = prompts.getPromptByName(name);
-    // console.log("name: ", name, "last", last);
+    // console.log("name: ", name, "last: ", last);
 
     const warning = !!last;
     setShowWarning(warning);
+    if (warning) {
+      setNameSignal(name);
+      // console.log("warning name:", name, "nameSignal():", nameSignal());
+    }
+    checkDisableAddButton();
+  }
+
+  function onInputText() {
+    checkDisableAddButton();
   }
 
   function onClickAdd() {
     // console.log("textarea.value:", textarea.value);
     const name = nameControl.value.trim();
     const text = textControl.value.trim();
+
     if (!name || !text || name.length === 0 || text.length === 0) {
+      console.log("name:", name, "text:", text);
       return;
     }
 
@@ -39,6 +52,16 @@ export default function AddPromptItem(props) {
 
     nameControl.value = "";
     textControl.value = "";
+    setDisableAddButton(true);
+  }
+
+  function checkDisableAddButton() {
+    const name = nameControl.value.trim();
+    const text = textControl.value.trim();
+    const disable = !name || !text || showWarning();
+
+    // console.log("!name", !name, "!text", !text, "showWarning", showWarning());
+    setDisableAddButton(disable);
   }
 
   return <>
@@ -55,11 +78,14 @@ export default function AddPromptItem(props) {
           </Col>
         </Row>
 
-        <Form.Control ref={textControl} as="textarea" rows={3} placeholder="Prompt text" />
-        <Alert variant="warning" show={showWarning()}>
-          A prompt with the same name already exists
-        </Alert>
-        <Button variant="outline-primary" size="sm" onClick={onClickAdd} disabled={showWarning()}>Add</Button>
+        <Form.Control ref={textControl} as="textarea" rows={3} placeholder="Prompt text" onInput={onInputText} />
+
+        <Row>
+          <h5>
+            <Badge bg="warning" hidden={!showWarning()}>The name="{nameSignal()}" already exists</Badge>
+          </h5>
+        </Row>
+        <Button variant="outline-primary" size="sm" onClick={onClickAdd} disabled={disableAddButton()}>Add</Button>
       </Card.Body>
     </Card>
   </>;
